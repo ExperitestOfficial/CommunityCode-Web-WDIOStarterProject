18c4695851d7a19c6135b1470be14a249416562f
alavi8680 <121008112+alavi8680@users.noreply.github.com>
2023-06-29 18:44:00 +0300

    Create wdio.conf.js

---
+++ wdio.conf.js
@@ -0,0 +1,30 @@
+exports.config = {
+    // ====================
+    // Runner Configuration
+    // ====================
+    hostname: 'mastercloud.experitest.com',
+    protocol: 'https',
+    port: 443,
+    path: '/wd/hub',
+    // ==================
+    // Specify Test Files
+    // ==================
+    // Define which test specs should run. The pattern is relative to the directory
+    // from which `wdio` was called.
+    specs: [
+        './test/specs/**/*.js'
+    ],
+    // ============
+    // Capabilities
+    // ============
+    capabilities: [{
+		'experitest:testName' : 'Demo Bank Example Test',
+		'experitest:accessKey' : <ACCESS_KEY>,
+		browserName: 'chrome', /* Change this to run your test on different browsers. */
+    }],
+
+    mochaOpts: {
+        ui: 'bdd',
+        timeout: 600000
+    },
+}
